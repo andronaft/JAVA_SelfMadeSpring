237893e797e9a22678000c09e1fe57b54ed2fc1b
andronaft <andronaft99@gmail.com>
2020-03-17 20:27:27 +0200

    some

--- src/com/zuk/Main.java
+++ src/com/zuk/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public Main(){
         BeanFactory beanFactory = new BeanFactory();
         beanFactory.instantiate("com.zuk");
+        ProductService productService = (ProductService) beanFactory.getBean("ProductService");
+        System.out.println(productService);
     }
 
 

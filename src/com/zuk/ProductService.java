c9b216635f0ad09cb0c5e86dc3fb6f31ee0d9117
andronaft <andronaft99@gmail.com>
2020-03-23 22:26:37 +0200

    add stereotype @Service , and @Autowired

--- src/com/zuk/ProductService.java
+++ src/com/zuk/ProductService.java
@@ -1,10 +1,13 @@
 package com.zuk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.stereotype.Component;
 
 @Component
 public class ProductService {
-    private PromotionsService promotionsService;
+
+    @Autowired
+    PromotionsService promotionsService;
 
     public PromotionsService getPromotionsService(){
         return promotionsService;
--- src/org/springframework/beans/factory/BeanFactory.java
+++ src/org/springframework/beans/factory/BeanFactory.java
@@ -1,9 +1,14 @@
 package org.springframework.beans.factory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.stereotype.Component;
+import org.springframework.beans.factory.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -36,7 +41,7 @@ public class BeanFactory {
 
                         Class classObject = Class.forName(basePackage + "." + className);
 
-                        if(classObject.isAnnotationPresent(Component.class)){
+                        if(classObject.isAnnotationPresent(Component.class)|| classObject.isAnnotationPresent(Service.class)){
                             System.out.println("Component: " + classObject);
                         }
                     }
@@ -46,6 +51,28 @@ public class BeanFactory {
 
         } catch (IOException | URISyntaxException | ClassNotFoundException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    public void populateProperties() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        System.out.println("==populateProperties==");
+
+
+        for (Object object : singletons.values()) {
+            for (Field field : object.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Autowired.class)) {
+
+                    for (Object dependency : singletons.values()) {
+                        if (dependency.getClass().equals(field.getType())) {
+                            String setterName = "set" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+                            System.out.println("Setter name = " + setterName);
+                            Method setter = object.getClass().getMethod(setterName, dependency.getClass());
+                            setter.invoke(object, dependency);
+                        }
+                    }
+                }
+            }
         }
 
     }
